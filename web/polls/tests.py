fb541b01ef64fe6ee1065087ef69193857f485ab
devkam <devkamura@gmail.com>
2025-11-07 14:31:09 +0900

    ✅ ビューのテストを実装

--- web/polls/tests.py
+++ web/polls/tests.py
@@ -2,15 +2,60 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
 class QuestionModelTests(TestCase):
-    """
-    未来の日付を入力した場合、False が返ってくること
-    """
     def test_was_published_recently_with_future_question(self):
+        """
+        未来の日付を入力した場合、False が返ってくることを確認するテスト
+        """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+
+def create_question(question_text, days):
+    """
+    未来の日付の質問を作成するメソッド
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        質問が存在しない場合、適切なメッセージが表示されることを確認するテスト
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"],[])
+    
+    def test_past_question(self):
+        """
+        pub_date フィールドが過去の日付の質問が表示されることを確認するテスト
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question])
+
+    def test_future_question(self):
+        """
+        pub_date フィールドが未来日の日付の質問が表示されないことを確認するテスト
+        """
+        question = create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+    
+    def test_two_past_questions(self):
+        """
+        pub_date フィールドが過去の日付の質問が複数表示されることを確認するテスト
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(response.context["latest_question_list"], [question2, question1])
 
